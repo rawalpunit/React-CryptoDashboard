9f73386807ca1b3f7301533b23724bdc16e88acf
rawalpunit <rawalhome@gmail.com>
2019-04-24 00:39:49 -0500

    Updates

---
+++ precache-manifest.18bc5f76f265f9833586d4d67302a3eb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0c52ef6393106a89a214733861e17ca",
+    "url": "/rawalpunit/React-CryptoDashboard/index.html"
+  },
+  {
+    "revision": "baf7336860130740f437",
+    "url": "/rawalpunit/React-CryptoDashboard/static/css/main.8a6e9829.chunk.css"
+  },
+  {
+    "revision": "3d0aab37eb68631b27b9",
+    "url": "/rawalpunit/React-CryptoDashboard/static/js/2.fc0ed5d0.chunk.js"
+  },
+  {
+    "revision": "baf7336860130740f437",
+    "url": "/rawalpunit/React-CryptoDashboard/static/js/main.f0b70163.chunk.js"
+  },
+  {
+    "revision": "3f9bee5ed8463d290ecb",
+    "url": "/rawalpunit/React-CryptoDashboard/static/js/runtime~main.c8735d90.js"
+  }
+]);
